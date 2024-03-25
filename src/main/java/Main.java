22de2d9ca9b6ebd5a1a9ea077ffc22602f22e32f
Borkley <115428985+borkleyspook@users.noreply.github.com>
2024-03-25 00:35:06 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    static final String futureRestoreGUIVersion = "1.98.3";
+    static final String futureRestoreGUIVersion = "1.98.4";
     static final boolean futureRestoreGUIPrerelease = false;
     public static void main(String[] args) {
         MainMenu.main();
